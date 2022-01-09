6ddb03fe66c3ca80bdc72e72d4c3d1b291db6fab
scottbisaillon <scott.bisaillon.dev@gmail.com>
2022-01-09 12:52:22 -0500

    refactor: change extension activation

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,21 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 import { newDartClass, newDartEnum, newFlutterStatefulWidget, newFlutterStatelessWidget } from "./commands";
+import { existsSync } from "fs";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	if (vscode.workspace.workspaceFolders !== undefined) {
+		let f = vscode.workspace.workspaceFolders[0].uri.fsPath;
+		const isDartProject = existsSync(f + "/pubspec.yaml");
+		vscode.commands.executeCommand("setContext", "ext.isDartProject", isDartProject);
+	}
+	else {
+		vscode.commands.executeCommand("setContext", "ext.isDartProject", false);
+	}
+
 	context.subscriptions.push(vscode.commands.registerCommand("dart-flutter-utils.dartNewClass", newDartClass));
 	context.subscriptions.push(vscode.commands.registerCommand("dart-flutter-utils.dartNewEnum", newDartEnum));
 	context.subscriptions.push(vscode.commands.registerCommand("dart-flutter-utils.flutterNewStatelessWidget", newFlutterStatelessWidget));
 	context.subscriptions.push(vscode.commands.registerCommand("dart-flutter-utils.flutterNewStatefulWidget", newFlutterStatefulWidget));
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
